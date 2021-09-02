d4ae3331db7707e0a77000009842be297c7afa9e
Lucas Gutierrez <70801680+LuGuDu@users.noreply.github.com>
2021-09-02 21:17:03 +0200

    Initial commit

---
+++ frmInicio.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Yahtzee
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEmpezar = new System.Windows.Forms.Button();
+            this.cbJugadores = new System.Windows.Forms.ComboBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEmpezar
+            // 
+            this.btnEmpezar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEmpezar.Location = new System.Drawing.Point(184, 318);
+            this.btnEmpezar.Name = "btnEmpezar";
+            this.btnEmpezar.Size = new System.Drawing.Size(199, 57);
+            this.btnEmpezar.TabIndex = 0;
+            this.btnEmpezar.Text = "Empezar";
+            this.btnEmpezar.UseVisualStyleBackColor = true;
+            // 
+            // cbJugadores
+            // 
+            this.cbJugadores.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbJugadores.FormattingEnabled = true;
+            this.cbJugadores.Items.AddRange(new object[] {
+            "2 Jugadores",
+            "3 Jugadores",
+            "4 Jugadores"});
+            this.cbJugadores.SelectedItem = "2 Jugadores";
+            this.cbJugadores.Location = new System.Drawing.Point(309, 238);
+            this.cbJugadores.Name = "cbJugadores";
+            this.cbJugadores.Size = new System.Drawing.Size(199, 24);
+            this.cbJugadores.TabIndex = 1;
+            this.cbJugadores.SelectedIndexChanged += new System.EventHandler(this.cbJugadores_SelectedIndexChanged);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.Location = new System.Drawing.Point(434, 318);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(199, 57);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.cbJugadores);
+            this.Controls.Add(this.btnEmpezar);
+            this.Name = "frmInicio";
+            this.Text = "Yahtzee";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEmpezar;
+        private System.Windows.Forms.ComboBox cbJugadores;
+        private System.Windows.Forms.Button btnSalir;
+
+    }
+}
+
---
+++ frmInicio.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yahtzee
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cbJugadores_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
